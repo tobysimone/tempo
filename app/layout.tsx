589f1db7bcb554e2e2380204c27be7f5ffb7ffc9
Toby Caulk <tobycaulk@gmail.com>
2023-10-25 10:02:29 -0400

    Add footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,15 @@ export default async function RootLayout({
             {children}
           </div>
         </main>
+        <div className="w-full flex flex-col items-center bg-white py-2.5 dark:border-gray-700 dark:bg-gray-800 sm:px-4 rounded">
+          <div className="flex justify-center text-center text-xs">
+            <p className="text-sm font-muted text-black dark:text-white">
+              Made with <span className="text-pink">&#9829;</span>
+              <br />
+              &copy; 2023
+            </p>
+          </div>
+        </div>
       </body>
     </html>
   )
