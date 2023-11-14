50c39866cefd35a09759645f1b2bf1fb45f54395
Toby Caulk <tobycaulk@gmail.com>
2023-11-14 08:50:49 -0500

    Add use-file-picker, let user select homepage header

--- app/artist/homepage/setup/page.tsx
+++ app/artist/homepage/setup/page.tsx
@@ -1,15 +1,22 @@
 'use client'
 
 import { FlowbiteTheme } from "@/app/_shared/theme/flowbite-theme";
+import Cropper from "@/components/cropper/Cropper";
 import { Button, Card, Modal } from "flowbite-react";
-import { useCallback, useEffect, useState } from "react";
+import { use, useCallback, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import Iframe from "react-iframe";
+import { useFilePicker } from "use-file-picker";
+import { ImageDimensionsValidator } from "use-file-picker/validators";
 
 export default function SetupArtistHomepage() {
     const [subdomain, setSubdomain] = useState<string>('');
     const [description, setDescription] = useState<string>('');
     const [showPreview, setShowPreview] = useState<boolean>(false);
+    const [showCropper, setShowCropper] = useState<boolean>(false);
     const [formChanged, setFormChanged] = useState<boolean>(false);
+    const [header, setHeader] = useState<string>('');
+    const [headerCropped, setHeaderCropped] = useState<boolean>(false);
 
     const getPreferences = useCallback(async () => {
         const response = await fetch('/api/artist/homepage/preferences');
@@ -67,34 +74,43 @@ export default function SetupArtistHomepage() {
                     </Button>
                 </div>
                 <hr />
-                <label htmlFor="title" className="mt-5 text-lg text-black dark:text-white">Homepage Link</label>
+                <label htmlFor="title" className="mt-5 text-lg text-black dark:text-white font-bold">Homepage Link</label>
                 <div className="flex flex-row items-center">
                     <input
                         name="title"
-                        type="text" 
-                        className="rounded-md px-4 bg-inherit border text-black dark:text-white" 
+                        type="text"
+                        className="rounded-md px-4 bg-inherit border text-black dark:text-white"
                         value={subdomain}
                         placeholder="bandname"
                         onChange={onSubdomainChange}
                     />
                     <span className="text-xl text-black dark:text-white">.tempo.com</span>
                 </div>
-                <label htmlFor="discription" className="mt-5 text-lg text-black dark:text-white">Artist Description</label>
+                <label htmlFor="discription" className="mt-5 text-lg text-black dark:text-white font-bold">Artist Description</label>
                 <textarea
                     name="description"
                     rows={6}
-                    className="rounded-md px-4 bg-inherit border text-black dark:text-white" 
+                    className="rounded-md px-4 bg-inherit border text-black dark:text-white"
                     value={description}
                     placeholder=""
                     onChange={onDescriptionChange}
                     style={{ resize: 'none' }}
                 />
+                <label className="mt-5 text-lg text-black dark:text-white font-bold">Header Image</label>
+                { header && headerCropped && (
+                    <img src={header} className="mt-3" style={{ }} />
+                )}
+                
+                <ImageHeaderPicker onImagePicked={(image: string) => {
+                    setHeader(image);
+                    setShowCropper(true);
+                }} />
 
                 { showPreview && (
-                    <Modal 
-                        show={showPreview} 
-                        size={'5xl'} 
-                        theme={FlowbiteTheme.MODAL_FULLSIZE} 
+                    <Modal
+                        show={showPreview}
+                        size={'5xl'}
+                        theme={FlowbiteTheme.MODAL_FULLSIZE}
                         onClose={() => setShowPreview(false)}
                     >
                         <Modal.Header>Preview</Modal.Header>
@@ -110,9 +126,72 @@ export default function SetupArtistHomepage() {
                         <Modal.Footer>
                             <Button onClick={() => setShowPreview(false)}>Close</Button>
                         </Modal.Footer>
-                    </Modal>    
+                    </Modal>
+                )}
+
+                { showCropper && (
+                    <Cropper
+                        size={'5xl'}
+                        show={showCropper}
+                        header={'Edit Header'}
+                        image={header}
+                        onCropComplete={(croppedHeader) => {
+                            setHeader(croppedHeader);
+                            setShowCropper(false);
+                            setHeaderCropped(true);
+                        }}
+                        onClose={() => {
+                            setShowCropper(false);
+                            setHeaderCropped(false);
+                        }}
+                        onError={(error: string) => {
+                            setShowCropper(false);
+                            setHeader('');
+                            setHeaderCropped(false);
+                            toast.error('Could not upload header, please try again');
+                            console.error(error);
+                        }}
+                    />
                 )}
             </Card>
         </>
+    )
+}
+
+function ImageHeaderPicker({ onImagePicked }: { onImagePicked: (image: string) => void }) {
+    const { openFilePicker, filesContent, loading, errors } = useFilePicker({
+        accept: 'image/*',
+        multiple: false,
+        readAs: 'DataURL',
+        limitFilesConfig: {
+            max: 1,
+            min: 1,
+            errorMessages: {
+                max: 'You can only upload one file',
+                min: 'You need to upload at least one file'
+            }
+        },
+        validators: [
+            new ImageDimensionsValidator({
+                maxWidth: 975,
+                minWidth: 0,
+                maxHeight: 180,
+                minHeight: 0
+            })
+        ]
+    });
+
+    useEffect(() => {
+        if(!filesContent || filesContent.length == 0) {
+            return;
+        }
+        
+        onImagePicked(filesContent[0].content);
+    }, [filesContent]);
+
+    return (
+        <a className="animated-link" onClick={openFilePicker} style={{ width: 'fit-content' }}>
+            Select Header Image
+        </a>
     )
 }
